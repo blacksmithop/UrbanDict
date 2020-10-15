c78a895eb59fc9eda7f57bbedf4f0ab17ab44f65
blacksmithop <angstycoder101@gmail.com>
2020-10-15 08:11:22 +0530

    push

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 
 setup(
-    name='UrbanDictPy',  # How you named your package folder (MyLib)
-    packages=['UrbanDictPy'],  # Chose the same as "name"
+    name='UrbanDictPython',  # How you named your package folder (MyLib)
+    packages=['UrbanDictPython'],  # Chose the same as "name"
     version='0.1',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='A simple Package to fetch **Urban Dictionary** definitions',  # Give a short description about your library
     author='BlackSmithOP',  # Type in your name
     author_email='angstycoder101@gmail.com',  # Type in your E-Mail
     url='https://github.com/blacksmithop/UrbanDict',  # Provide either the link to your github or to your website
-    download_url='https://github.com/blacksmithop/UrbanDict/archive/v_01.tar.gz',  # I explain this later on
+    download_url='https://github.com/blacksmithop/UrbanDict/archive/v_0_1.tar.gz',  # I explain this later on
     keywords=['UrbanDictionary', 'UrbanDict', 'UrbanDictPy'],  # Keywords that define your package best
     install_requires=[  # I get to this in a second
     ],
